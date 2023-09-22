e82d3e9000eb0577fec152646d7f16c5f7d52211
gustavo21332 <pindamom54@gmail.com>
2023-09-22 13:31:13 +0000

    gfvbgcfgfgdfgfgd

---
+++ app/src/main/java/application/model/Plataforma.java
@@ -0,0 +1,40 @@
+package application.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="plataformas")
+
+public class Plataforma {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String fornecedor;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getFornecedor() {
+        return fornecedor;
+    }
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+    
+    
+}
